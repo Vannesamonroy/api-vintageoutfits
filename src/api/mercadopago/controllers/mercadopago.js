7c4223e08da292c713b5ebd38069fae2ad35561a
Vannesa <vanessamonroy546@gmail.com>
2024-05-20 09:24:52 -0500

    agregando mercado pago endpoint

--- src/api/mercadopago/controllers/mercadopago.js
+++ src/api/mercadopago/controllers/mercadopago.js
@@ -1,5 +1,9 @@
 'use strict';
-
+const { MercadoPagoConfig, Preference } = require("mercadopago");
+const client = new MercadoPagoConfig({
+  accessToken:
+    "TEST-7089495801591210-051620-c8afd387b8b58488e4e40a298da393fa-208171607",
+});
 /**
  * A set of functions called "actions" for `mercadopago`
  */
@@ -7,10 +11,31 @@
 module.exports = {
   exampleAction: async (ctx, next) => {
      try {
-      console.log(ctx.request.body);
-       ctx.body = 'ok';
+      const data = ctx.request.body;
+      const body = {
+        items: [
+          {
+            title: data.title,
+            quantity: Number(data.quantity),
+            unit_price: Number(data.price),
+            currency_id: "COP",
+          },
+        ],
+        back_urls: {
+          success: "http://localhost:5173",
+          failure: "http://localhost:5173",
+          pending: "http://localhost:5173",
+        },
+        auto_return: "approved",
+      };
+      const preference = new Preference(client);
+      const result = await preference.create({ body });
+      ctx.body = {
+        id:result.id
+       };
      } catch (err) {
-       ctx.body = err;
+      console.log(err)
+       ctx.body = {error:"Error al crear preference"};
      }
    }
 };
