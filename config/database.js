f0822a767461e3a121f4f7ab89ff8260a876d9ee
Vannesa <vanessamonroy546@gmail.com>
2024-06-06 14:50:21 -0500

    agregando posgres

--- config/database.js
+++ config/database.js
@@ -1,16 +1,33 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
+      host: env("DATABASE_HOST", "monorail.proxy.rlwy.net"),
+      port: env.int("DATABASE_PORT", 24500),
       database: env("DATABASE_NAME", "store"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", false),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "BQYUraWHERkuSkLoxzUJfdwWuICQfsLh"),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
     },
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "mysql",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 3306),
+//       database: env("DATABASE_NAME", "store"),
+//       user: env("DATABASE_USERNAME", "root"),
+//       password: env("DATABASE_PASSWORD", ""),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//   },
+// });
 
 
 // module.exports = ({ env }) => ({
